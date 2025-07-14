fd3b385b18beb475a0b0f8cd3ee8a786819b6a2c
geodeguzman <22-70720@g.batstate-u.edu.ph>
2025-07-14 16:15:44 +0800

    updated functions.php

--- qrcodeweb/functions.php
+++ qrcodeweb/functions.php
@@ -1,8 +1,17 @@
 <?php
 require 'db.php';
 
-function getData($conn) {
-    $result = $conn->query("SELECT * FROM qr_links ORDER BY id DESC");
-    return $result->fetch_all(MYSQLI_ASSOC);
+function getData($conn, $search = null) {
+    if ($search) {
+        $stmt = $conn->prepare("SELECT * FROM qr_links WHERE title LIKE ? ORDER BY id DESC");
+        $like = "%$search%";
+        $stmt->bind_param("s", $like);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    } else {
+        $result = $conn->query("SELECT * FROM qr_links ORDER BY id DESC");
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
 }
 ?>
