7407ecc5d7cf6dd841297fc0b91e3beab6510fa5
AceMercado <22-07491@g.batstate-u.edu.ph>
2025-07-14 15:00:02 +0800

    Update functions.php

--- qrcodeweb/functions.php
+++ qrcodeweb/functions.php
@@ -1,1 +1,8 @@
-func
+<?php
+require 'db.php';
+
+function getData($conn) {
+    $result = $conn->query("SELECT * FROM qr_links ORDER BY id DESC");
+    return $result->fetch_all(MYSQLI_ASSOC);
+}
+?>
